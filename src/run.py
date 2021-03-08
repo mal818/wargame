fbb8f1c882770f90a9eccc50a227bc5d62f07d8c
maliang <maliang@email.cn>
2021-03-08 22:58:08 +0800

    提交源代码

---
+++ src/run.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+# @Time    :2021/3/8 21:54
+# @Author  :MaLiang
+# @Email   :mal818@126.com
+# @File    :run.py
+
+from orcwargame import attackoftheorcs
+attackoftheorcs.AttackOfTheOrcs().play()
